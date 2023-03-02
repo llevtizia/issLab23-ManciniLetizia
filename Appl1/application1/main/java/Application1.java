b86cca3569dc0d3e9080fe42a73f5dbc3d4fea7d
Letizia Mancini <38510317+llevtizia@users.noreply.github.com>
2023-03-02 18:01:19 +0100

    update

--- Appl1/application1/main/java/Application1.java
+++
@@ -1,8 +0,0 @@
-import unibo.basicomm23.utils.CommUtils;
-package unibo;
-public class Application1 {
-        public static void main( String[] args ){
-            System.out.println("Application1 hello");
-            CommUtils.outblue("Application1 hello again");
-        }
-}
