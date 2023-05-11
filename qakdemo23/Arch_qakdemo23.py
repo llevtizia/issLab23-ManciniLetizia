36a7cd2048b3c51ac05a90b71f7f00b96da32222
Letizia Mancini <38510317+llevtizia@users.noreply.github.com>
2023-05-11 14:48:50 +0200

    update

--- qakdemo23/Arch_qakdemo23.py
+++ qakdemo23/Arch_qakdemo23.py
@@ -19,8 +19,12 @@ eventedgeattr = {
 with Diagram('qakdemo23Arch', show=False, outformat='png', graph_attr=graphattr) as diag:
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
-     with Cluster('ctxdemoreq', graph_attr=nodeattr):
-          caller=Custom('caller','./qakicons/symActorSmall.png')
-          called=Custom('called','./qakicons/symActorSmall.png')
-     caller >> Edge(color='magenta', style='solid', xlabel='r1', fontcolor='magenta') >> called
+     with Cluster('ctxdemo0', graph_attr=nodeattr):
+          demo0=Custom('demo0','./qakicons/symActorSmall.png')
+          perceiver=Custom('perceiver','./qakicons/symActorSmall.png')
+          sender=Custom('sender','./qakicons/symActorSmall.png')
+     sys >> Edge(color='red', style='dashed', xlabel='alarm', fontcolor='red') >> perceiver
+     sender >> Edge(color='blue', style='solid', xlabel='msg1', fontcolor='blue') >> demo0
+     sender >> Edge(color='blue', style='solid', xlabel='msg2', fontcolor='blue') >> demo0
+     sender >> Edge( xlabel='alarm', **eventedgeattr, fontcolor='red') >> sys
 diag
--- qakdemo23/qakdemo23.pl
+++ qakdemo23/qakdemo23.pl
@@ -1,7 +1,7 @@
 %====================================================================================
 % qakdemo23 description   
 %====================================================================================
-context(ctxdemoreq, "localhost",  "TCP", "8010").
- qactor( caller, ctxdemoreq, "it.unibo.caller.Caller").
-  qactor( called, ctxdemoreq, "it.unibo.called.Called").
-msglogging.
+context(ctxdemo0, "localhost",  "TCP", "8095").
+ qactor( demo0, ctxdemo0, "it.unibo.demo0.Demo0").
+  qactor( perceiver, ctxdemo0, "it.unibo.perceiver.Perceiver").
+  qactor( sender, ctxdemo0, "it.unibo.sender.Sender").
