8a9da7d72e43eeecc20a99ae730307e9ce579cd8
Letizia Mancini <38510317+llevtizia@users.noreply.github.com>
2023-06-02 10:08:50 +0200

    update

--- sonarQak23/Arch_sonarqak23.py
+++ sonarQak23/Arch_sonarqak23.py
@@ -19,4 +19,7 @@ eventedgeattr = {
 with Diagram('sonarqak23Arch', show=False, outformat='png', graph_attr=graphattr) as diag:
   with Cluster('env'):
      sys = Custom('','./qakicons/system.png')
+     with Cluster('ctxsonarqak23', graph_attr=nodeattr):
+          sonar23=Custom('sonar23','./qakicons/symActorSmall.png')
+          appl=Custom('appl','./qakicons/symActorSmall.png')
 diag
--- sonarQak23/sonarqak23.pl
+++ sonarQak23/sonarqak23.pl
@@ -2,4 +2,6 @@
 % sonarqak23 description   
 %====================================================================================
 context(ctxsonarqak23, "localhost",  "TCP", "8020").
-context(ctxbasicrobot, "127.0.0.1",  "TCP", "8020").
+context(ctxbasicrobot, "127.0.0.1",  "TCP", "8021").
+ qactor( sonar23, ctxsonarqak23, "it.unibo.sonar23.Sonar23").
+  qactor( appl, ctxsonarqak23, "it.unibo.appl.Appl").
